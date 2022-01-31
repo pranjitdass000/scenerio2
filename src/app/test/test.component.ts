dd8b8e87d20f5af95e2222067a54e62c3a1ad59e
Pranjit <pranjit.dass000@gmail.com>
2022-01-31 10:21:45 +0530

    first commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl:'./test.component.html',
+  styles: ['']
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+  public  successClass={
+  }
+  public name = "";
+
+  ngOnInit(): void {
+  }
+
+}
